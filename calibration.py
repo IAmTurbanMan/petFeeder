218b64e0a2d95c746aa587f2118de997a5ea6415
IAmTurbanMan <rashidul.kader@gmail.com>
2020-08-12 21:37:27 -0700

    playing around with redundancy for data in case of data loss somewhere

--- calibration.py
+++ calibration.py
@@ -3,6 +3,9 @@
 # calibration file to run	#
 # as a first time setup or 	#
 # or when changing bowls.	#
+# built in data redundancy	#
+# with json file in case 	#
+# of data loss			#
 #				#
 # written by: Rash Kader	#
 #	      Wesley Crank	#
@@ -13,12 +16,15 @@
 #!/usr/bin/python
 
 import ADC
+import json
+import os
 from configparser import ConfigParser as cp
 from time import sleep
 
-#define variables and list
-waterChannel = 0
-foodChannel = 1
+#define variables
+channel = 0
+selection = ''
+section = ''
 readingsList = []
 
 #create configparser object
@@ -59,9 +65,8 @@ def resetConfig (section, setting):
 	config_ini[section][setting] = '0'
 
 #menu
-selection = ''
-
 while True:
+	os.system('clear')
 	print('''
 **********************************
  Welcome to weight calibration.  *
@@ -76,45 +81,75 @@ while True:
 
 	selection = input('Selection: ')
 	if selection == '1':
+		section = 'WATERBOWL'
+		channel = 0
 		#asks user to place empty bowl on water FSR, then waits for user to press Enter
 		#runs function to write avg value to config.ini file
 		a = input('Please place your empty water bowl on the sensor then press Enter.')
-		writeConfig(waterChannel, 'WATERBOWL', 'empty_bowl')
+		writeConfig(channel, section, 'empty_bowl')
 
 		#asks user to fill bowl to a low level, then waits for user to press Enter
 		#same as above
 		a = input('Please fill your bowl to a low level then press Enter.')
-		writeConfig(waterChannel, 'WATERBOWL', 'low_water')
+		writeConfig(channel, section, 'low_water')
 
 		#asks user to fill bowl to a half level, then waits for user to press Enter
 		#same as above
 		a = input('Please fill your bowl to half level then press Enter.')
-		writeConfig(waterChannel, 'WATERBOWL', 'half_full_bowl')
+		writeConfig(channel, section, 'half_full_bowl')
 
 		#asks user to fill bowl to full level, then waits for user to press Enter
 		#same as above
 		a = input('Please fill your bowl to full level then press Enter.')
-		writeConfig(waterChannel, 'WATERBOWL', 'full_bowl')
+		writeConfig(channel, section, 'full_bowl')
 
 		commitChanges()
 
+		empty_bowl = config_ini[section]['empty_bowl']
+		low_water = config_ini[section]['low_water']
+		half_full_bowl = config_ini[section]['half_full_bowl']
+		full_bowl = config_ini[section]['full_bowl']
+		waterMeasurements = {
+			"empty_bowl":empty_bowl,
+			"low_water":low_water,
+			"half_full_bowl":half_full_bowl,
+			"full_bowl":full_bowl
+			}
+
+		with open ('waterMeasurements.json','w') as write_file:
+			json.dump(waterMeasurements, write_file)
+
 	if selection == '2':
+		section = 'FOODBOWL'
+		channel = 1
 		#asks user to place empty bowl on food FSR, waits for user to press Enter
 		#runs function to write avg value to config.ini file
 		a = input('Please place your empty food bowl on the sensor then press Enter.')
-		writeConfig(foodChannel, 'FOODBOWL', 'empty_bowl')
+		writeConfig(channel, section, 'empty_bowl')
 
 		#asks user to fill bowl half full, waits for user to press Enter
 		#writes to config.ini
 		a = input('Please fill your bowl to half level then press Enter.')
-		writeConfig(foodChannel, 'FOODBOWL', 'half_full_bowl')
+		writeConfig(channel, section, 'half_full_bowl')
 
 		#asks user to fill bowl to full level, waits for user to press Enter
 		#writes to config.ini
 		a = input('Please fill your bowl to full level then press Enter.')
-		writeConfig(foodChannel, 'FOODBOWL', 'full_bowl')
+		writeConfig(channel, section, 'full_bowl')
 
 		commitChanges()
+
+		empty_bowl = config_ini[section]['empty_bowl']
+		half_full_bowl = config_ini[section]['half_full_bowl']
+		full_bowl = config_ini[section]['full_bowl']
+		foodMeasurements = {
+			"empty_bowl":empty_bowl,
+			"half_full_bowl":half_full_bowl,
+			"full_bowl":full_bowl
+			}
+
+		with open ('foodMeasurements.json','w') as write_file:
+			json.dump(foodMeasurements, write_file)
 
 	if selection == '3':
 		resetConfig('WATERBOWL','empty_bowl')
