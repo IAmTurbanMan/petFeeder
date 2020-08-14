9f2bc064d73e9f86c9135942780ea8aadfdc4a61
IAmTurbanMan <rashidul.kader@gmail.com>
2020-08-14 11:45:47 -0700

    relay.py: Work on parsing config.ini and and reading values from ADC

--- relay.py
+++ relay.py
@@ -11,10 +11,57 @@
 
 #!/usr/bin/python
 
+import os
 import ADC
-from configparser import ConfigParser as cp
+import threading
+import RPi.GPIO as GPIO
 from time import sleep
+from configparser import ConfigParser as cp
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+pins = [2,3]
+waterThresh = []
+foodThresh = []
+
+#Relay pin setup
+#Must be set up as inputs, so relays stay off
+GPIO.setup(pins[0],GPIO.IN)
+GPIO.setup(pins[1],GPIO.IN)
 
 #create configparser object
 config_ini = cp()
 config_ini.read('./config.ini')
+
+#function to read config.ini and return variables
+#and add them to a list
+def thresholds(section,setting, list):
+	value = config_ini[section][setting]
+	list.append(int(value))
+
+thresholds('WATERBOWL','empty_bowl',waterThresh)
+thresholds('WATERBOWL','low_water',waterThresh)
+thresholds('WATERBOWL','half_full_bowl',waterThresh)
+thresholds('WATERBOWL','full_bowl',waterThresh)
+
+thresholds('FOODBOWL','empty_bowl',foodThresh)
+thresholds('FOODBOWL','half_full_bowl',foodThresh)
+thresholds('FOODBOWL','full_bowl',foodThresh)
+
+#define threads
+#sensor1Thread = threading.Thread(target=ADC.adcMeasure, args=(0,))
+#sensor2Thread = threading.Thread(target=ADC.adcMeasure, args=(1,))
+#start threads
+#sensor1Thread.start()
+#sensor2Thread.start()
+
+while True:
+	os.system('clear')
+	sensor1 = ADC.adcMeasure(0)
+	sensor2 = ADC.adcMeasure(1)
+#	print (waterThresh)
+#	print (foodThresh)
+	print (sensor1)
+	print (sensor2)
+	sleep(.5)
