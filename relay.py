1bab585fd7e0ee227da5bc7f1ca154ac9a206844
IAmTurbanMan <rashidul.kader@gmail.com>
2020-08-14 17:49:57 -0700

    Worked on threading functions

--- relay.py
+++ relay.py
@@ -24,6 +24,10 @@ GPIO.setwarnings(False)
 pins = [2,3]
 waterThresh = []
 foodThresh = []
+relayOn = ['off','off']
+running = 'y'
+sensor1 = 0
+sensor2 = 0
 
 #Relay pin setup
 #Must be set up as inputs, so relays stay off
@@ -49,19 +53,42 @@ thresholds('FOODBOWL','empty_bowl',foodThresh)
 thresholds('FOODBOWL','half_full_bowl',foodThresh)
 thresholds('FOODBOWL','full_bowl',foodThresh)
 
-#define threads
-#sensor1Thread = threading.Thread(target=ADC.adcMeasure, args=(0,))
-#sensor2Thread = threading.Thread(target=ADC.adcMeasure, args=(1,))
-#start threads
-#sensor1Thread.start()
-#sensor2Thread.start()
+#function to tell realy 1 to switch on or off depending on
+#threshholds
+def waterRelaySwitch():
+	global sensor1, sensor2
+	global relayOn, waterThresh
+	while running != 'n':
+		if (sensor1 < waterThresh[1] and relayOn[0] == 'off'):
+			relayOn[0] = 'on'
+		elif (sensor1 > waterThresh[3] and relayOn[0] == 'on'):
+			relayOn[0] = 'off'
 
-while True:
+#function to poll FSRs
+def polling(seconds):
+	global sensor1, sensor2
+	while running != 'n':
+		sensor1 = ADC.adcMeasure(0)
+		sensor2 = ADC.adcMeasure(1)
+		sleep(seconds)
+
+pollThread = threading.Thread(target=polling,args=(1,))
+pollThread.start()
+waterThread = threading.Thread(target=waterRelaySwitch)
+waterThread.start()
+
+while running != 'n':
+#	sensor1 = ADC.adcMeasure(0)
+#	sensor2 = ADC.adcMeasure(1)
 	os.system('clear')
-	sensor1 = ADC.adcMeasure(0)
-	sensor2 = ADC.adcMeasure(1)
-#	print (waterThresh)
-#	print (foodThresh)
 	print (sensor1)
 	print (sensor2)
-	sleep(.5)
+
+	if (relayOn[0] == 'on'):
+		print (relayOn[0])
+		GPIO.setup(pins[0],GPIO.OUT)
+	elif (relayOn[0] == 'off'):
+		print (relayOn[0])
+		GPIO.setup(pins[0],GPIO.IN)
+
+	sleep(1)
